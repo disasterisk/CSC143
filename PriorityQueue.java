1171b62446ac673c87df606bfe2b4c24ed3014f0
nkruszewski <34694695+nkruszewski@users.noreply.github.com>
2018-06-05 16:04:31 -0700

    Add files via upload

---
+++ PriorityQueue.java
@@ -0,0 +1,42 @@
+import java.util.List;
+
+public class PriorityQueue<E extends Comparable<E>> {
+
+
+    public PriorityQueue() {
+    }
+
+    public void insert(E e) {
+
+    }
+
+    public E remove() {
+
+        //Change this
+        return null;
+    }
+
+    public E front() {
+
+        //Change this
+        return null;
+    }
+
+    public boolean isEmpty() {
+
+        //change this
+        return true;
+    }
+
+    public List<E> testForwardTraversal() {
+
+        //change this
+        return null;
+    }
+
+    public List<E> testReverseTraversal() {
+
+        //change this
+        return null;
+    }
+}
